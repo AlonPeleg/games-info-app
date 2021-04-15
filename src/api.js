3b9f0e421148b3ca99ac2e9ef9a50ec66201efee
Alon Peleg <35157275+AlonPeleg@users.noreply.github.com>
2021-04-15 23:54:24 +0300

    starting to work on the reducer

---
+++ src/api.js
@@ -0,0 +1,27 @@
+// Base URL -
+const base_url = "https://api.rawg.io/api/";
+
+// Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) return `0${month}`;
+  return month;
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) return `0${day}`;
+  return day;
+};
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Popular Games
+const popular_games = `games?key=${process.env.REACT_APP_RAWG_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
